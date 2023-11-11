48052e208cd7a2601fb755f9cd40d17c9bd68ac4
Sayyed Sharuk <sayyedsharuk75@gmail.com>
2023-11-11 09:51:13 +0530

    Contact Form update

--- src/pages/GithubStats/GithubStats.tsx
+++ src/pages/GithubStats/GithubStats.tsx
@@ -1,4 +1,4 @@
-import { Flex, Image, Text } from "@chakra-ui/react";
+import { Flex, Image } from "@chakra-ui/react";
 import GitHubCalendar from "react-github-calendar";
 import { fadeTop } from "../../animations/FadeAnimations";
 
