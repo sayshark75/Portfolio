2a62bd55d5fcab872c6b7b54415163c0601cc01a
Sayyed Sharuk <sayyedsharuk75@gmail.com>
2023-12-25 12:45:27 +0530

    LCP Reduced with static compressed assets

--- src/sources/Certificates.ts
+++ src/sources/Certificates.ts
@@ -1,20 +1,12 @@
-export const cert_AIHealthCare = "https://github.com/sayshark75/sayshark75/assets/112304655/e2ae2d52-d980-42b5-af8f-c07bf7227b39";
-
-export const cert_CSS = "https://github.com/sayshark75/sayshark75/assets/112304655/386eadc5-6b01-4f7a-a2e7-293e7753dac4";
-
-export const cert_Express = "https://github.com/sayshark75/sayshark75/assets/112304655/9dcb1f28-da1d-4fa2-94a8-a9f716b5a299";
-
-export const cert_HTML5 = "https://github.com/sayshark75/sayshark75/assets/112304655/e50aadae-217d-47a3-9edf-8e8634fa2ad4";
-
-export const cert_JSAdvanced = "https://github.com/sayshark75/sayshark75/assets/112304655/8575c656-0c11-4743-89d0-431af577143f";
-
-export const cert_MongoDB = "https://github.com/sayshark75/sayshark75/assets/112304655/912ab00c-b09b-4cc0-bd48-e5814348d2b6";
-
-export const cert_ProgWebApps = "https://github.com/sayshark75/sayshark75/assets/112304655/512cec2b-e7c3-44f7-8592-f54ff94f2740";
-
-export const cert_React = "https://github.com/sayshark75/sayshark75/assets/112304655/a0a6f549-87db-4bf6-8cab-1ff9e982f6da";
-
-export const cert_TypeScript = "https://github.com/sayshark75/sayshark75/assets/112304655/e308d894-9159-42b9-9300-ef3146e26439";
+import cert_CSS from "../assets/certificates/CSS.jpg";
+import cert_React from "../assets/certificates/ReactJS.jpg";
+import cert_AIHealthCare from "../assets/certificates/AIHealthCare.jpg";
+import cert_Express from "../assets/certificates/ExpressJS.jpg";
+import cert_HTML5 from "../assets/certificates/HTML5.jpg";
+import cert_ProgWebApps from "../assets/certificates/ProgWebAppDev.jpg";
+import cert_MongoDB from "../assets/certificates/MDB.jpg";
+import cert_JSAdvanced from "../assets/certificates/JSAdv.jpg";
+import cert_TypeScript from "../assets/certificates/Typescript.jpg";
 
 export const certificates = [
   { image: cert_CSS, tag: "CSS" },
